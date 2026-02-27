87400a70f96dacbaf2d6d3857835b4cf50a4d778
ecccXu <xcs_yeaheo@yeah.net>
2026-02-27 20:13:07 +0800

    - 修改

--- backend/config/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-配置模块初始化
-"""
-from .app_config import config
-from .db_config import db
-
-__all__ = ['config', 'db']
--- backend/models/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-数据库模型模块初始化
-"""
-from backend.config.db_config import db, SystemStatus, Sensor, SensorConfig, MqttConfig, TestCase, TestResult, SensorData
-
-__all__ = ['db', 'SystemStatus', 'Sensor', 'SensorConfig', 'MqttConfig', 'TestCase', 'TestResult', 'SensorData']
